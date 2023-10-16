a2ee38521b77add28f6a29c7e8798702f7b6d774
sharabidinov <alymbeksharabidinov1@gmail.com>
2023-10-17 02:47:24 +0600

    solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0) {
+            switch (month) {
+                case 2:
+                    System.out.println(28);
+                    break;
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.println(31);
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.println(30);
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        } else {
+            switch (month) {
+                case 2:
+                    System.out.println(29);
+                    break;
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.println(31);
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.println(30);
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        String result = switch (monthNumber) {
+            case 12, 1, 2:
+                yield "Winter";
+            case 3,4,5:
+                yield "Spring";
+            case 6,7,8:
+                yield "Summer";
+            case 9, 10, 11:
+                yield "Autumn";
+            default:
+                yield "Wrong month number";
+        };
+        System.out.println(result);
     }
 }
